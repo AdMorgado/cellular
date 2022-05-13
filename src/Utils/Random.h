16f63a6e02c4fa388618c6f1a842628f8ba31b34
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-05-13 17:05:36 +0100

    add rudimentary random and math utils

---
+++ src/Utils/Random.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <random>
+
+float random01()
+{
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+float random(float min, float max)
+{
+    const float interval = max - min;
+
+    return min + (random01() / interval);
+}
+
