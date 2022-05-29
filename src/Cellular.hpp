f45de70eb47d55776e95c893a3f0589bea60d8c9
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-05-29 01:12:54 +0100

    add utils headers

--- src/Cellular.hpp
+++ src/Cellular.hpp
@@ -1,11 +1,14 @@
 #pragma once
 
 #include "Core/Layer.hpp"
-#include "Core/Application.hpp"
 
 #include "Scene/Scene.hpp"
 #include "Scene/System.hpp"
 #include "Scene/Entity.hpp"
 #include "Scene/Component.hpp"
+
+#include "Utils/Math.h"
+#include "Utils/Random.h"
+
 
 
