451dc81317056a21514b80b6b8c96e58b7bf3d87
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-06-26 20:51:03 +0100

    degToRad and radToDeg funcs

--- src/Utils/Math.h
+++ src/Utils/Math.h
@@ -2,6 +2,12 @@
 
 #include <math.h>
 
+constexpr double PI = 3.14159265358979323846;
 
+constexpr inline double degToRad(double& degrees) noexcept {
+    return degrees * (PI / 180.0);
+}
 
-
+constexpr inline double radToDeg(double& radians) noexcept {
+    return radians * (180.0 / PI); 
+}
