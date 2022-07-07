4fe7a19d7127e6986cf4147119953c6cb0d30d98
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-07-07 22:20:23 +0100

    move implementations into cpp file

---
+++ src/Threads/JobQueue.cpp
@@ -0,0 +1,36 @@
+#include <JobQueue.hpp>
+
+
+void JobQueue::enqueue(Job* job) {
+    std::unique_lock<std::mutex> lock(m_mut);
+    m_queue.push(job);
+    m_cond.notify_one();
+}
+
+Job* JobQueue::dequeue(std::chrono::milliseconds timeout) {
+    std::unique_lock<std::mutex> lock(m_mut);
+
+    if(!m_queue.empty()) {
+        Job* job = m_queue.front();
+        m_queue.pop();
+        return job;
+    }
+
+    while(true) 
+    {  
+        std::cv_status status = m_cond.wait_for(lock, timeout);
+
+        switch(status) {
+            case std::cv_status::no_timeout:
+                if(!m_queue.empty()) {
+                    Job* job = m_queue.front();
+                    m_queue.pop();
+                    return job;
+                }
+                break;
+            case std::cv_status::timeout:
+                return nullptr;
+                break;
+        }
+    }
+}
