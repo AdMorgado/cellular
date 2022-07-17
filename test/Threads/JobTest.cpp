824873bb94ff6dbc24173934dc168fc2d3ceec05
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-07-17 03:55:15 +0100

    add exhautive text

--- test/Threads/JobTest.cpp
+++ test/Threads/JobTest.cpp
@@ -1,6 +1,8 @@
 
 #include <gtest/gtest.h>
 
+#include <thread>
+
 #include "Cellular.hpp"
 
 TEST(JobTest, ValidUse) {
@@ -13,4 +15,17 @@ TEST(JobTest, InvalidUse) {
     Job job([](){});
     ASSERT_NO_THROW(job.execute());
     ASSERT_THROW(job.execute(), std::runtime_error);
+}
+
+constexpr int NUM_OF_REPS = 10000;
+
+TEST(JobTest, ExhaustiveTest) {
+    for(int i = 0; i < NUM_OF_REPS; i++) {
+        Job job([](){});
+        std::thread thr([&job = job](){
+            job.execute();
+        });
+        job.join();
+        thr.join();
+    }
 }
