f92dc0e5cfba570adb15b61e807ef98809bac6d3
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-07-24 17:49:38 +0100

    add imgui sfml base integration

--- src/Core/Application.cpp
+++ src/Core/Application.cpp
@@ -1,24 +1,22 @@
 #include <Core/Application.hpp>
 
-#include "Threads/ThreadPool.hpp"
 
 #include <chrono>
+#include <string>
 #include <iostream>
 
+#include "imgui.h"
+#include "imgui-SFML.h"
 
-App::App()
-{
-    const auto numOfThreads = std::thread::hardware_concurrency();
-    m_threadPool.createThreads(numOfThreads);
+App::App() : 
+    m_threadPool(std::thread::hardware_concurrency()) {
 }
 
-App::~App()
-{
+App::~App() {
     m_threadPool.shutdown();
 }
 
-void App::setActiveLayer(Layer* layer)
-{
+void App::setActiveLayer(Layer* layer) {
     if(!layer) return;
 
     m_layer = layer;
@@ -30,17 +28,22 @@ void App::run() {
     m_window.create(sf::VideoMode(960, 640), "cellular");
     m_window.setFramerateLimit(60);
 
+    ImGui::SFML::Init(m_window);
+
     bool isRunning = true;
     sf::Clock internalClock;
     while(isRunning) {
         // Input
         sf::Event event;
         while (m_window.pollEvent(event)) {
+            ImGui::SFML::ProcessEvent(m_window, event);
+
             if (event.type == sf::Event::Closed)
                 isRunning = false;
         }
-
-        const float deltaTime = internalClock.restart().asSeconds();
+        const sf::Time dt = internalClock.restart();
+        const float deltaTime = dt.asSeconds();
+        ImGui::SFML::Update(m_window, dt);
 
         // Update
         if(m_layer) {
@@ -49,14 +52,17 @@ void App::run() {
             }, deltaTime);
         }
 
+        // Render
         m_window.clear();
-
         if(m_layer) {
             m_layer->render(m_window);
+            m_layer->renderUI();
         }
 
+        ImGui::SFML::Render(m_window);
         m_window.display();
     }
 
     m_window.close();
+    ImGui::SFML::Shutdown();
 }
