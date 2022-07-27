5e78767716f18c2eed162f55764e815ecd461316
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-07-27 17:32:09 +0100

    update example with tested functionality

--- examples/ParticlesLayer.cpp
+++ examples/ParticlesLayer.cpp
@@ -1,6 +1,7 @@
 
 #include <Cellular.hpp>
 
+#include <mutex>
 #include <thread>
 
 struct Particle {
@@ -8,45 +9,61 @@ struct Particle {
     float radius            {1.f};
 };
 
-struct ParticleSystem : public System {
-    virtual void update(Scene& scene) override {
-
-    }
-};
-
-struct ParticleRendererSystem : public System {
-    virtual void render(const Scene& scene, sf::RenderTarget& target) const override {
-
-    }
-};
-
 class ParticlesLayer : public Layer {
 public:
+    ParticlesLayer() {
+        
+    }
+    virtual ~ParticlesLayer() {
+        
+    }
+
     virtual void start() override {
-        //systems.push_back(new ParticleSystem());
+        
+        std::lock_guard<std::mutex> lock(guard);
+        shape.setTexture(Resource::getTexture("textures/circle.png"));
+        
     }
 
     virtual void update(std::function<void(Job*)> dispatchJob, const float dt) override {
-        
-        Job job([&systems = systems, &scene = scene](){
+        Job job([&shape = shape, &guard = guard, dt]{
+            std::lock_guard<std::mutex> lock(guard);
+            sf::Vector2f move;
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) move += sf::Vector2f( 0.f, -1.f);
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) move += sf::Vector2f( 0.f,  1.f);
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) move += sf::Vector2f(-1.f,  0.f);
+            if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) move += sf::Vector2f( 1.f,  0.f);
 
+            shape.move(move * dt * 150.f);
         });
-        //dispatchJob(&job);
         job.join();
     }
 
     virtual void render(sf::RenderTarget& target) override {
-        renderer.render(scene, target);
+        std::lock_guard<std::mutex> lock(guard);
+        target.draw(shape);
+
+        sf::Text text;
+        text.setFont(Resource::getFont("fonts/ProggyClean.ttf"));
+        text.setString("dsfgerg");
+        text.setPosition(0.f, 0.f);
+        text.setCharacterSize(24);
+        text.setFillColor(sf::Color::Red);
+        target.draw(text);
     }
 
     virtual void renderUI() override {
         
     }
 
+    virtual void handleEvent(const sf::Event& event) override {
+
+    }
+
 private:
-    Scene scene;
-    std::vector<System*> systems;
-    ParticleRendererSystem renderer;
+
+    std::mutex guard;
+    sf::Sprite shape;
 };
 
 Layer* getLayer() {
