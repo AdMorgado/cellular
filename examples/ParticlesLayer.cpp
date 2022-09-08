ddb3dbbe6ae78e6854ddcfbf42acaa9c522e8590
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-09-08 22:04:28 +0100

    remove undefined variable

--- examples/ParticlesLayer.cpp
+++ examples/ParticlesLayer.cpp
@@ -20,36 +20,15 @@ public:
 
     virtual void start() override {
         
-        std::lock_guard<std::mutex> lock(guard);
-        shape.setTexture(Resource::getTexture("textures/circle.png"));
         
     }
 
     virtual void update(std::function<void(Job*)> dispatchJob, const float dt) override {
-        Job job([&shape = shape, &guard = guard, dt]{
-            std::lock_guard<std::mutex> lock(guard);
-            sf::Vector2f move;
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::W)) move += sf::Vector2f( 0.f, -1.f);
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) move += sf::Vector2f( 0.f,  1.f);
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::A)) move += sf::Vector2f(-1.f,  0.f);
-            if(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) move += sf::Vector2f( 1.f,  0.f);
 
-            shape.move(move * dt * 150.f);
-        });
-        job.join();
     }
 
     virtual void render(sf::RenderTarget& target) override {
-        std::lock_guard<std::mutex> lock(guard);
-        target.draw(shape);
 
-        sf::Text text;
-        text.setFont(Resource::getFont("fonts/ProggyClean.ttf"));
-        text.setString("dsfgerg");
-        text.setPosition(0.f, 0.f);
-        text.setCharacterSize(24);
-        text.setFillColor(sf::Color::Red);
-        target.draw(text);
     }
 
     virtual void renderUI() override {
@@ -61,8 +40,6 @@ public:
     }
 
 private:
-
-    std::mutex guard;
     sf::Sprite shape;
 };
 
