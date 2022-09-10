b16a61be93cc32ca5a0e1d3db251be08e9f78315
AdMorgado <73718959+AdMorgado@users.noreply.github.com>
2022-09-11 00:14:57 +0100

    implement random funcs in source file

---
+++ src/Utils/Random.cpp
@@ -0,0 +1,15 @@
+#include "Utils/Random.hpp"
+
+float random01()
+{
+    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+float random(float min, float max)
+{
+    const float interval = max - min;
+
+    return min + (random01() * interval);
+}
+
+
--- src/Utils/Random.hpp
+++ src/Utils/Random.hpp
@@ -2,15 +2,6 @@
 
 #include <random>
 
-float random01()
-{
-    return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-}
-
-float random(float min, float max)
-{
-    const float interval = max - min;
-
-    return min + (random01() * interval);
-}
+float random01();
+float random(float min, float max);
 
